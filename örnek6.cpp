8b392a244b56e85ae13e540a2777e0cedf4d535a
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-10-31 14:07:48 +0300

    Add files via upload

---
+++ örnek6.cpp
@@ -0,0 +1,16 @@
+//Rümeysa Coþkun
+//Program amacý:cout<<endl ve \n kullanýmýný göstermektedir.
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Hello Rumeysa";
+cout<<"Hello Dikey";
+cout<<endl;
+cout<<"Hello Ayse \n";
+cout<<"Hello Hayriye"<<"Hello Fatma"<<endl;
+cout<<"Hello \n \n";
+cout<<"World";
+return 0;
+system("PAUSE");	
+}
